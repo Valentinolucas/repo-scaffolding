8f92d8f31e8fcead882fa68d3de17bd9df3f15a7
Lucas Fantini <valentinolucasfantini@gmail.com>
2023-11-28 14:58:39 +0000

    feat: build and push image

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -7,7 +7,7 @@ set -o pipefail
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Build the Docker image
-# docker build -t "$DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG" .
+docker build -t "$DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG" .
 
 # # Push the Docker image to Docker Hub
 # docker push "$DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
